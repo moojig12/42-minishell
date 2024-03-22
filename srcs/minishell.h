b5161abaecc981a484abbbe708b680ecf068a032
moojig12 <moojig12>
2024-03-22 17:49:45 +0100

    start

---
+++ srcs/minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmandakh <nmandakh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/22 17:44:09 by nmandakh          #+#    #+#             */
+/*   Updated: 2024/03/22 17:49:11 by nmandakh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINI_SHELL_H
+# define MINI_SHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
