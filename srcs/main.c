aef500b473ef157e739daf148b26b2ba5ec631f9
moojig12 <moojig12>
2024-03-22 19:05:01 +0100

    Small change to start

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: nmandakh <nmandakh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 17:43:58 by nmandakh          #+#    #+#             */
-/*   Updated: 2024/03/22 17:48:38 by nmandakh         ###   ########.fr       */
+/*   Updated: 2024/03/22 17:55:49 by nmandakh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,7 @@ int	main(void)
 	while (1)
 	{
 		input = readline("minishell:");
+		if (input == NULL)
+			exit (0);
 	}
 }
--- srcs/minishell.h
+++ srcs/minishell.h
@@ -6,17 +6,21 @@
 /*   By: nmandakh <nmandakh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 17:44:09 by nmandakh          #+#    #+#             */
-/*   Updated: 2024/03/22 17:49:11 by nmandakh         ###   ########.fr       */
+/*   Updated: 2024/03/22 17:53:15 by nmandakh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINI_SHELL_H
-# define MINI_SHELL_H
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
+# include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <unistd.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <dirent.h>
+# include <termios.h>
 
 #endif
