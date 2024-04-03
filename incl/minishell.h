c90c98db9c6bbbaa527e3ae5d6ab39d5e9c42e4f
moojig12 <moojig12>
2024-04-03 18:39:32 +0200

    added old tokenization to main

--- incl/minishell.h
+++ incl/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yjinnouc <yjinnouc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nmandakh <nmandakh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 17:44:09 by nmandakh          #+#    #+#             */
-/*   Updated: 2024/03/29 12:23:14 by yjinnouc         ###   ########.fr       */
+/*   Updated: 2024/04/03 18:38:57 by nmandakh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,7 @@ typedef struct s_token {
 	char			*value;
 	int				type;
 	struct 	s_token *next;
+	struct	s_token *prev;
 }	t_token;
 
 typedef struct s_hist {
