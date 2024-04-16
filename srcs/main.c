224490b788cac55a4f5284fbc8f8d503422b4318
Yuki Jinnouchi (Jini) <yuki.jinnouchi@gmail.com>
2024-04-16 19:32:08 +0000

    fix a bit

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: yjinnouc <yjinnouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 17:43:58 by nmandakh          #+#    #+#             */
-/*   Updated: 2024/04/16 19:49:42 by yjinnouc         ###   ########.fr       */
+/*   Updated: 2024/04/16 19:50:47 by yjinnouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	main(int argc, char **argv, char **env)
 {
 	// int		token_count;
 	char	*input;
-	int		token_count;
+	// int		token_count;
 	t_token	*tokens;
 
 	if (argc != 1 || argv[1] != NULL)
@@ -36,7 +36,8 @@ int	main(int argc, char **argv, char **env)
 		if (*input)
 			add_history(input);
 		tokens = NULL;
-		token_count = lexical_analysis(&tokens, input);
+		// token_count = 
+		lexical_analysis(&tokens, input);
 		print_tokens(tokens);
 		execute(tokens, env);
 		free_token(tokens);
