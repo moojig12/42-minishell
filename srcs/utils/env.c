b11ddbd90939c83c17245888373fc980b1155eb8
moojig12 <moojig5152@gmail.com>
2024-07-10 16:33:55 +0200

    new branch?

--- srcs/utils/env.c
+++ srcs/utils/env.c
@@ -6,42 +6,85 @@
 /*   By: nmandakh <nmandakh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 14:17:25 by yjinnouc          #+#    #+#             */
-/*   Updated: 2024/07/10 15:32:18 by nmandakh         ###   ########.fr       */
+/*   Updated: 2024/07/10 16:33:21 by nmandakh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	count_vars(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str)
+	{
+		if (*str == '$')
+			i++;
+		str++;
+	}
+	return (i);
+}
+
+int	extract_varlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && (!ft_isspace(str[i]) || str[i] != '$'))
+		i++;
+	return (i);
+}
+
+void	len_pop(char *str, int *len)
+{
+	int	i;
+	int	j;
+
+	j = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i++] == '$')
+		{
+			len[j++] = extract_varlen(&str[i]);
+		}
+	}
+}
+
+int	*count_varlen(char *str)
+{
+	int	*len;
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	len = (int *)malloc((count_vars(str)) * sizeof(int));
+	len_pop(str, len);
+	return (len);
+}
+
+char	*replace_vars(char *str, char **var, int *len)
+{
+	char	*result;
+
+	result = (char *)malloc(((ft_strlen(str) - orig_var()) + int_sum()) * sizeof(char))
+}
 // replace env variable when str[start] is '$'
 // FIX: fixed memleak but too messy
 char	*replace_env_var(char *str, int start, t_values *vals)
 {
-	int		key_len;
-	char	*key;
-	char	*val;
-	char	*replaced;
-	int		start_latter;
-	char	*first;
+	char	**var;
+	char	*result;
+	int		*len;
+	int		i;
 
-	if (str[start + 1] == '?')
-	{
-		key_len = 1;
-		key = ft_strdup("?");
-		val = ft_itoa(vals->last_exit_code);
-	}
-	else
-	{
-		key_len = count_env_key(str + start + 1);
-		key = ft_substr(str, start + 1, key_len);
-		val = ft_strdup(get_env_str(vals->env, key));
-	}
-	free(key);
-	start_latter = start + key_len + 1;
-	first = ft_substr(str, 0, start);
-	replaced = ft_3strjoin(first, val, str + start_latter);
-	free(str);
-	free(val);
-	free(first);
-	return (replaced);
+	if (!str[start + 1] || ft_isspace(str[start + 1]))
+		return (str);
+	len = count_varlen(str);
+	var = (char **)malloc((count_vars(str)) * sizeof(char *));
+	result = replace_vars(str, var, len);
+	return (result);
 }
 
